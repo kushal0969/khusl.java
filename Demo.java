5e9df2bc4e417a31f3dd1bf5290a77d29fcba9d2
kushal khadka <kushalkhadka2025@gmail.com>
2026-01-06 12:10:55 +0545

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package Assignment2;
+public class Demo {
+    int x;
+    boolean flag;
+
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println(d.x);
+        System.out.println(d.flag);
+    }
+}
